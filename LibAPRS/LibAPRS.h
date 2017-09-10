d9797bb285fe4d95a6ba8959447186cedc12626b
Tim Ahpee <tim@ahpee.org>
2017-09-10 21:53:37 +1000

    Added #defines for the MIC-E Standard Messages

--- LibAPRS/LibAPRS.h
+++ LibAPRS/LibAPRS.h
@@ -8,6 +8,15 @@
 #include "AFSK.h"
 #include "AX25.h"
 
+#define MICE_STD_MSG_OFF_DUTY 0x07
+#define MICE_STD_MSG_EN_ROUTE 0x06
+#define MICE_STD_MSG_IN_SERVICE 0x05
+#define MICE_STD_MSG_RETURNING 0x04
+#define MICE_STD_MSG_COMMITTED 0x03
+#define MICE_STD_MSG_SPECIAL 0x02
+#define MICE_STD_MSG_PRIORITY 0x01
+#define MICE_STD_MSG_EMERGENCY 0x00
+
 void APRS_init(int reference, bool open_squelch);
 void APRS_poll(void);
 
